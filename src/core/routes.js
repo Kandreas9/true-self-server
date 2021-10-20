403d0bc5d281792950f4ae85e20074ce19f987d9
Kandreas9 <andkarido@gmail.com>
2021-10-20 20:22:23 +0300

    fix(core/routes): Fixed name in routes

--- src/core/routes.js
+++ src/core/routes.js
@@ -3,7 +3,7 @@ import truthMessageRouter from '../modules/truthMessage/Router';
 
 const routes = (app) => {
     app.use('/base', baseRouter);
-    app.use('/truthMessage', truthMessageRouter);
+    app.use('/truth-message', truthMessageRouter);
 };
 
 export default routes;
